291aa2fa03aed6c9ad8ec69a51b778b5a2a8b6ba
brodvir <121065366+brodvir@users.noreply.github.com>
2022-12-20 13:17:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let menuIcon = document.getElementById("icon_menu");
+let navbar = document.getElementById("navbar");
+menuIcon.addEventListener("click", function(){
+    navbar.classList.toggle("active")
+}, false) 
+
